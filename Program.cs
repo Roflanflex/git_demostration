f47693e6d8f93d4f48259426cb391d65008fed72
Grenvime <Prokaznikjealous@yandex.ru>
2023-09-03 20:29:03 +0300

    a

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace git_demostration
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("9000!");
+            Console.WriteLine("a");
         }
     }
 }
