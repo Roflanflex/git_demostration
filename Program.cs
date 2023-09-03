5d1bca8c55defca0f9787a3577d5f1b35285ac7f
Grenvime <Prokaznikjealous@yandex.ru>
2023-09-03 19:53:20 +0300

    changed to 858

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace git_demostration
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("80");
+            Console.WriteLine("858");
         }
     }
 }
